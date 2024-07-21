05c5275610946cfb6bc3ce15a3630810012f24d1
EkaterinaKopenkina <kopenkina_ekaterina@bk.ru>
2024-07-21 15:12:14 +0300

    блюр-formContact

--- src/assets/js/contact.js
+++ src/assets/js/contact.js
@@ -9,12 +9,17 @@ const formContact = () => {
         const thisForm = document.querySelector('.contact__form');
         const inputs = thisForm.querySelectorAll('.contact__input');
 
+        const error = thisForm.querySelector('error__form');
+        const success = thisForm.querySelector('success__form');
+
         const name = document.querySelector('#contactName');
         const email = document.querySelector('#contactEmail');
         const rEmail = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i;
         const message = document.querySelector('#contactMessage');
 
         form.formClear(inputs);
+        error.style.display = 'none';
+        success.style.display = 'none';
 
         if (name.value == '') {
             form.formError(name, 'Пустое поле');
@@ -24,7 +29,7 @@ const formContact = () => {
 
         if (!rEmail.test(email.value)) {
             form.formError(email, 'Поле заполнено неверно');
-            form.formEmailBlur(email);
+            form.formRBlur(email, rEmail);
             return;
         }
 
