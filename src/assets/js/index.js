e3c11588fb6d09c416dfd2239445eb4ade75c1c3
EkaterinaKopenkina <kopenkina_ekaterina@bk.ru>
2024-07-21 15:17:31 +0300

    очистка-сообщений-formNewsletter

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -23,11 +23,16 @@ const formNewsletter = () => {
         const thisForm = document.querySelector('.newsletter__form');
         const inputs = thisForm.querySelectorAll('.newsletter__input');
 
+        const error = thisForm.querySelector('.newsletter__error-form');
+        const success = thisForm.querySelector('.newsletter__success-form');
+
         const name = document.querySelector('#newsletterName');
         const email = document.querySelector('#newsletterEmail');
         const rEmail = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i;
 
         form.formClear(inputs);
+        error.style.display = 'none';
+        success.style.display = 'none';
 
         if (name.value == '') {
             form.formError(name, 'Пустое поле');
