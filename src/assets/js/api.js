f6292f44e95ef6a7725da3759d2f3a67ebb97e7d
EkaterinaKopenkina <kopenkina_ekaterina@bk.ru>
2024-07-26 11:58:52 +0300

    функция-deleteData

--- src/assets/js/api.js
+++ src/assets/js/api.js
@@ -38,3 +38,18 @@ const postData = async (url, data) => {
 
     return await response.json();
 }
+
+const deleteData = async (url, data) => {
+    const response = await fetch(url, {
+        method: 'DELETE',
+        headers: {
+            'X-API-KEY': 'FC52783F63184532B379EECD56DFC009E0131854354C4FA293EC5581CC6547F7',
+            'Authorization': 'Bearer ' + getToken(),
+        },
+        body: data,
+    });
+
+    if (!response.ok) {
+        throw new Error(response.status);
+    }
+}
