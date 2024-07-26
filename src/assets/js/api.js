189f200de40177a3564073834f7d84530e723308
EkaterinaKopenkina <kopenkina_ekaterina@bk.ru>
2024-07-26 11:55:55 +0300

    функция-getData

--- src/assets/js/api.js
+++ src/assets/js/api.js
@@ -5,3 +5,19 @@ const getToken = () => {
     }
     return '';
 }
+
+const getData = async (url) => {
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'X-API-KEY': 'FC52783F63184532B379EECD56DFC009E0131854354C4FA293EC5581CC6547F7',
+            'Authorization': 'Bearer ' + getToken(),
+        }
+    });
+
+    if (!response.ok) {
+        throw new Error(response.status);
+    }
+
+    return await response.json();
+}
